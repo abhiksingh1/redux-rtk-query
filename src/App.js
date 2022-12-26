4124e8918a4541ae8267998f3a36bc8ec129c810
Abhimanyu Kumar <abhimanyukumar82982@gmail.com>
2022-12-26 23:10:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+
+}
+
+export default App;
